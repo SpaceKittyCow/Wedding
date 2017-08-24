dfaaa33147a268db6a8f9d79fc3984ed61682ccb
Toki <SpaceKittyCow@users.noreply.github.com>
2017-08-23 20:06:11 -0400

    Working composer and main page
    POST has been mocked along with adding a mock config file.
    Phpunit had to be removed for prodcution due to blue host
    not updating the local php version above 5.6. Should
    be added later back for dev.

--- index.php
+++ index.php
@@ -1,22 +1,39 @@
 <?php 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/config.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
+
 $app->get('/', function() use($app) { 
-return readfile("template.html");
+   return readfile("template.html");
 });
+
 $app->get('/hello/{name}', function($name) use($app) {
     return 'Hello '.$app->escape($name);
 });
+
 $app->post('/submit', function(Request $request) {
-    $data =json_decode($request->getContent(), true);
+    $data = json_decode($request->getContent(), true);
     try {
-        $Attendee = new Wedding\Attendee($data['state']);
-       $state = $user->SetPiece($column);
+        $verified = verifyGoogleRecaptcha($data[''], $config)
+	if ($verfied == true) {
+	    $attendee = new Wedding\Attendee($data['name'], $config['database']);
+	    $attendee->isComing($data[''])
+            if ($attendee->isComing()){
+        	$attendee->PartyCount($data['']);
+    		return "We're so excited your coming";
+	    } else {
+    		return "We appriciate your response. So sorry you can't make it.";
+	    }
+
+	} else {
+            return "You didn't click the I'm not a robot correctly. Try again." 
+	}
     } catch (\Exception $e) {
-       return $e;
+	error_log($e, 3, $config["logs"]);
+       return "There was an internal error. Please try again in a bit";
     }
-    return json_encode($state);
 });
+
 $app->run();
