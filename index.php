0454a17b287870a5255337bd71fdb4b3158c5e48
Toki <SpaceKittyCow@users.noreply.github.com>
2017-08-28 01:10:09 -0400

    Front end now POST
    Clean up and other infomation now in.

--- index.php
+++ index.php
@@ -5,19 +5,21 @@ use Silex\Provider\FormServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
-
-$app->get('/', function() use($app) { 
-   return readfile("template.html");
-});
+$loader = new Twig_Loader_Filesystem('./');
+$twig = new Twig_Environment($loader, array());
 
 $app->get('/hello/{name}', function($name) use($app) {
-    
     return 'Hello '.$app->escape($name);
 });
+
+$app->get('/', function() use($app, $twig) { 
+   return $twig->render('template.html', array()); 
+});
+
 $app->post('/submit', function(Request $request, Silex\Application $app) {
-    require_once __DIR__.'/config.php';
+    require_once __DIR__.'/config.php'; // I hate this here
     try {
-//        $verified = verifyGoogleRecaptcha($data['g-recaptcha-response'], $config)
+//      $verified = verifyGoogleRecaptcha($data['g-recaptcha-response'], $config)
 //	if ($verfied == true) {
 	    $attendee = new Wedding\Attendee($app->escape($request->get('Name')), $config['database']);
 	    $attendee->isAttending($app->escape($request->get('RSVP')));
@@ -25,9 +27,9 @@ $app->post('/submit', function(Request $request, Silex\Application $app) {
             $attendee->commit();
 
             if ($attendee->isAttending()){
-    		return "We're so excited your coming";
+    		return "We're glad you can make it.";
 	    } else {
-    		return "We appriciate your response. So sorry you can't make it.";
+    		return "We'll miss you.";
 	    }
 
 //	} else {
@@ -35,7 +37,7 @@ $app->post('/submit', function(Request $request, Silex\Application $app) {
 //	}
     } catch (\Exception $e) {
 	error_log($e, 3, $config["logs"]);
-       return "There was an internal error. Please try again in a bit";
+       return "There was an Error, please try again in a bit or email help@fuzzy-ideas.com.";
     }
 });
 
